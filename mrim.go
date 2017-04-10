2466335dce429407b545efc93dec5d8a15db2694
Vladimir Varankin <nek.narqo+git@gmail.com>
2017-04-10 13:23:11 +0300

    fixup! extract stuff into mrim.Client

---
+++ mrim.go
@@ -0,0 +1,239 @@
+package mrim
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+var (
+	ErrNoHello = errors.New("no hello")
+)
+
+const (
+	DefaultInitTimeout = 30 * time.Second
+)
+
+type Options struct {
+	Addr       string
+	Username   string
+	Password   string
+	Status     uint32
+	ClientDesc string
+}
+
+type Client struct {
+	conn   *Conn
+	logger *log.Logger
+
+	loginAddr net.Addr
+
+	clientDesc string
+	// helloAck becomes true after MRIM_CS_HELLO_ACK retrived.
+	helloAck bool
+}
+
+func NewClient(ctx context.Context, opt *Options) (*Client, error) {
+	logger := log.New(os.Stderr, "mrim: ", log.Lshortfile)
+	c := &Client{
+		logger:     logger,
+		clientDesc: opt.ClientDesc,
+	}
+
+	err := c.Connect(ctx, opt.Addr)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.Hello(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.Auth(ctx, opt.Username, opt.Password, opt.Status)
+	if err != nil {
+		return nil, err
+	}
+
+	// after this point conn is meant to be established, run the conn reader
+	c.conn.Run()
+
+	return c, nil
+}
+
+func (c *Client) Connect(ctx context.Context, address string) error {
+	if c.conn != nil {
+		return errors.New("mrim: already connected")
+	}
+	return c.connect(ctx, address)
+}
+
+func parseLoginAddr(data []byte) (net.Addr, error) {
+	d := bytes.IndexByte(data, ':')
+	if d == -1 {
+		return nil, fmt.Errorf("bad login addr: %v", data)
+	}
+	host, data := data[:d], data[d+1:]
+	port, err := strconv.ParseUint(string(data[:4]), 10, 16)
+	if err != nil {
+		return nil, fmt.Errorf("bad login addr: %v", data)
+	}
+
+	ip := net.ParseIP(string(host))
+	loginAddr := &net.TCPAddr{
+		IP:   ip,
+		Port: int(port),
+	}
+	return loginAddr, nil
+}
+
+// connect initializes net connection to login host retrieved from server address.
+// TODO(varankinv): refactor connect()
+func (c *Client) connect(ctx context.Context, address string) error {
+	nconn, err := net.DialTimeout("tcp", address, DefaultInitTimeout)
+	if err != nil {
+		return err
+	}
+	defer nconn.Close()
+
+	data := make([]byte, 24)
+	if _, err := nconn.Read(data); err != nil {
+		return err
+	}
+	loginAddr, err := parseLoginAddr(data)
+	if err != nil {
+		return err
+	}
+
+	c.logger.Printf("loggin addr: %s\n", loginAddr.String())
+
+	conn, err := Dial(ctx, loginAddr.String())
+	if err != nil {
+		return fmt.Errorf("could not dial to login addr: %v", err)
+	}
+
+	c.loginAddr = loginAddr
+	c.conn = conn
+
+	return nil
+}
+
+func (c *Client) Close() error {
+	if c.conn == nil {
+		return nil
+	}
+	err := c.conn.Close()
+	if err != nil {
+		c.conn = nil
+	}
+	return nil
+}
+
+// Hello sends "MRIM_CS_HELLO" message and reads the reply.
+func (c *Client) Hello(ctx context.Context) (err error) {
+	if c.helloAck {
+		return errors.New("mrim: repeative hello call")
+	}
+	return c.hello(ctx)
+}
+
+func (c *Client) hello(ctx context.Context) (err error) {
+	if c.helloAck {
+		return nil
+	}
+
+	var p Packet
+	p.Header.Msg = MsgCSHello
+
+	err = c.conn.Send(ctx, p)
+	if err != nil {
+		return err
+	}
+
+	// read reply here because readLoop hasn't been started yet.
+	p, err = c.conn.ReadPacket()
+	if err != nil {
+		return err
+	}
+
+	if p.Msg != MsgCSHelloAck {
+		return PacketError{p, errUnknownPacket}
+	}
+
+	c.helloAck = true
+
+	pingInterval := binary.LittleEndian.Uint32(p.Data)
+	c.logger.Printf("> received \"MRIM_CS_HELLO_ACK\" packet: %d, %04x, ping %d\n", p.Seq, p.Msg, pingInterval)
+
+	if pingInterval > 0 {
+		// FIXME(varankinv): think of a better way of setting pingInterval.
+		c.conn.mu.Lock()
+		c.conn.pingInterval = time.Duration(pingInterval) * time.Second
+		c.conn.mu.Unlock()
+	}
+
+	return nil
+}
+
+type AuthError struct {
+	s string
+}
+
+func (e AuthError) Error() string {
+	return e.s
+}
+
+// Auth sends "MRIM_CS_LOGIN2" and reads the reply.
+func (c *Client) Auth(ctx context.Context, username, password string, status uint32) (err error) {
+	if !c.helloAck {
+		return ErrNoHello
+	}
+
+	pw := PacketWriter{}
+	pw.WriteData(username)
+	pw.WriteData(password)
+	pw.WriteData(status)
+	pw.WriteData(c.clientDesc)
+	for i := 0; i < 5; i++ {
+		pw.WriteData(0) // internal fields
+	}
+	err = c.conn.Send(ctx, pw.Packet(MsgCSLogin2))
+	if err != nil {
+		return err
+	}
+
+	// read reply here because readLoop hasn't been started yet.
+	p, err := c.conn.ReadPacket()
+	if err != nil {
+		return err
+	}
+
+	switch p.Msg {
+	case MsgCSLoginAck:
+		c.logger.Printf("> received \"MRIM_CS_LOGIN_ACK\" packet: %d, %04x\n", p.Seq, p.Msg)
+
+	case MsgCSLoginRej:
+		reason, err := unpackLPS(p.Data)
+		if err != nil {
+			return fmt.Errorf("mrim: cound not read auth rejection reason: %v", err)
+		}
+		c.logger.Printf("> received \"MRIM_CS_LOGIN_REJ\" packet: %d, %04x, reason %q\n", p.Seq, p.Msg, reason)
+		return AuthError{reason}
+
+	default:
+		return PacketError{p, errUnknownPacket}
+	}
+
+	return nil
+}
+
+func (c *Client) Send(ctx context.Context, p Packet) error {
+	return c.conn.Send(ctx, p)
+}
